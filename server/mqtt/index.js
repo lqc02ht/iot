01783ac9e2a306c2ff29f03d99376760947d0f5a
hungtran_20021371 <duyhungtran716@gmail.com>
2024-12-25 10:26:10 +0700

    build docker container

--- server/mqtt/index.js
+++ server/mqtt/index.js
@@ -3,32 +3,34 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
-export const mqttClient = mqtt.connect(
-  `mqtt://${process.env.MQTT_SERVER}:${process.env.MQTT_PORT}`,
-  {
-    clientId,
-    username: process.env.MQTT_USERNAME,
-    password: process.env.MQTT_PASSWORD,
-  }
-);
+export const mqttClient = mqtt.connect(`mqtt://${process.env.MQTT_SERVER}:${process.env.MQTT_PORT}`, {
+  clientId,
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+});
 
 const topic = [1, 2, 3];
+const sensorTypes = ["fire", "temperature", "gas"];
 
-mqttClient.on('connect', () => {
+mqttClient.on("connect", () => {
   topic.forEach((t) => {
-    console.log(`Subscribing to topic: /esp/${t}/gas`);
-    mqttClient.subscribe(`/esp/${t}/gas`);
+    sensorTypes.forEach((type) => {
+      const topic = `/esp/${sensorId}/${type}`;
+      console.log(`Subscribing to topic: ${topic}`);
+      mqttClient.subscribe(topic);
+    });
   });
-})
+});
 
 // Handle errors
-mqttClient.on('error', (err) => {
-  console.error('MQTT error:', err);
+mqttClient.on("error", (err) => {
+  console.error("MQTT error:", err);
 });
 
 // Handle MQTT disconnection
-mqttClient.on('close', () => {
-  console.log('MQTT connection closed. Reconnecting...');
+mqttClient.on("close", () => {
+  console.log(process.env.MQTT_USERNAME);
+  console.log(process.env.MQTT_PASSWORD);
+  console.log("MQTT connection closed. Reconnecting...");
   mqttClient.reconnect();
 });
-
